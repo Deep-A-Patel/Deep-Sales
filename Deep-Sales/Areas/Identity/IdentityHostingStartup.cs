49532b15a6d585f3ac7aa3152e15e2c42306bb02
Deep-A_Patel <deep.ag123007@gmail.com>
2019-09-06 08:37:05 -0700

    login and register and logout is working with FirstName and LastName

---
+++ Deep-Sales/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Deep_Sales.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Deep_Sales.Areas.Identity.IdentityHostingStartup))]
+namespace Deep_Sales.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
