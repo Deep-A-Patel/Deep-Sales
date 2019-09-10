f6d5105860338fb707b0e16a030682939bb80f73
Deep-A_Patel <deep.ag123007@gmail.com>
2019-09-10 08:14:06 -0700

    phone number issue is solved in registration

--- Deep-Sales/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Deep-Sales/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -79,7 +79,7 @@ namespace Deep_Sales.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, PhoneNumber = Input.PhoneNumber };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
